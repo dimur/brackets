bc5f4657e76d099093fb7f2fd57ef5664b3fcbb2
Dmitry Murzin <d.a.murzin@gmail.com>
2020-10-10 08:55:25 +0300

    feat: add solution to the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let lifo = []
+  const brackets = new Map(bracketsConfig);
+
+  for (const element of str.split('')) {
+    if (brackets.has(element) && lifo[lifo.length - 1] !== brackets.get(element)) lifo.push(element)
+    else
+      if ([...brackets.values()].includes(element) && brackets.get(lifo.pop()) !== element)
+        return false
+  }
+
+  if (lifo.length === 0) return true
+  else return false
 }
